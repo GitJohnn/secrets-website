76be295260a2f5351708ec8bfb98b82440efc846
GitJohnn <39565975+GitJohnn@users.noreply.github.com>
2020-07-23 15:52:37 -0500

    Environment variables module added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 require("dotenv").config();
+const dotenv=require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
